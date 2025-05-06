4def70d92941fc2ed4f0984536a3692acc44d02a
Rey-R06 <rlliiirollooo@gmail.com>
2025-05-05 19:30:36 -0500

    fix(router): resolver fallo de rutas de la pagina de  registro

--- src/router/enrutador.jsx
+++ src/router/enrutador.jsx
@@ -1,7 +1,7 @@
 import Home from "../pages/home/Home"
 import Login from "../pages/login/Login"
 import Inicio from "../pages/inicio/Inicio"
-import Registrarse from "../registrarse/Registrarse"
+import Registrarse from "../pages/registrarse/Registrarse" 
 import Productos from "../pages/productos/Productos"
 
 export let enrutador = [
